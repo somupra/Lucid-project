e9077e9bf5c5a57611d24c7353c69357200e841a
muskangarg21 <50917120+muskangarg21@users.noreply.github.com>
2019-07-10 19:04:40 +0530

    Update views.py

--- saarthi/mun_dashboard/views.py
+++ saarthi/mun_dashboard/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from users.models import Notification
 
 # Create your views here.
 @login_required
@@ -22,6 +23,7 @@ def approve_success(request, pk):
     complaint.filer.profile.rewards = points
     complaint.filer.profile.save()
     complaint.save()
+    notify(request, points, complaint)
     messages.success(request, f'Complaint verified successfully!')
     return render(request, 'mun_dashboard/approved_complaint.html', {'complaint': complaint, 'reward': points})
 
@@ -44,7 +46,11 @@ def solved_complaints(request):
     complaints = Complaint.objects.filter(is_settled= True).order_by('-date_filed')
     return render(request, 'mun_dashboard/solved_complaints.html', {'complaints': complaints})
 
-
+@login_required
+def notify(request, points, complaint):
+    message = "Your complaint with ID " + complaint.complaint_id +" has been verified and you have been rewarded " + points +" points. "
+    notif = Notification(notification=message, complaint=complaint)
+    notif.save()
 
 
 
